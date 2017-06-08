6f2429d829b9857ac417f956f5a327db3247e145
sun-yukun@qq.com <>
2017-06-08 17:45:18 +0800

    clean

--- src/main/java/net/coderdaily/bean/Post.java
+++ src/main/java/net/coderdaily/bean/Post.java
@@ -1,64 +1,78 @@
 package net.coderdaily.bean;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.mongodb.core.index.Indexed;
+
 public class Post {
-    private String id;
-    @Indexed(unique = true)
-    private String title;
-    private Date date;
-    private List comments;
+	private String id;
+	@Indexed(unique = true)
+	private String title;
+	private String content;
+	private Date date;
+	private List comments;
 
-    public Post(String title, Date date) {
-        this.title = title;
-        this.date = date;
-    }
+	public Post(String title, Date date) {
+		this.title = title;
+		this.date = date;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public Post() {
+		super();
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public Post(String title, String content, Date date) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.date = date;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public Date getDate() {
-        return date;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public List getComments() {
-        return comments;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public void setComments(List comments) {
-        this.comments = comments;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", date=" + date +
-                ", comments=" + comments +
-                '}';
-    }
+	public List getComments() {
+		return comments;
+	}
+
+	public void setComments(List comments) {
+		this.comments = comments;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", title=" + title + ", content=" + content + ", date=" + date + ", comments="
+				+ comments + "]";
+	}
 
 }
--- src/test/java/MongoTest.java
+++ src/test/java/MongoTest.java
@@ -8,32 +8,48 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import net.Application;
 import net.coderdaily.bean.Post;
+import net.coderdaily.util.FileUtil;
 import net.coerdaily.post.dao.PostRepostitory;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class MongoTest {
-    @Autowired
-    private PostRepostitory postRepository;
+	@Autowired
+	private PostRepostitory postRepository;
 
-    @Autowired
-    MongoTemplate mongoTemplate;
+	@Autowired
+	MongoTemplate mongoTemplate;
 
-    @Test
-    public void testList() {
-        postRepository.save(new Post("ddd",new Date()));
+	@Test
+	public void testList() {
+		postRepository.save(new Post("ddd", new Date()));
 
-        Post post = postRepository.findByTitle("aaa");
-        System.out.println(post);
+		Post post = postRepository.findByTitle("aaa");
+		System.out.println(post);
 
+		List<Post> post2 = postRepository.findAll();
+		System.out.println(post2);
 
-        List<Post> post2 = postRepository.findAll();
-        System.out.println(post2);
+		List<Post> post3 = mongoTemplate.findAll(Post.class);
+		System.out.println(post3);
+	}
 
-        List<Post> post3 = mongoTemplate.findAll(Post.class);
-        System.out.println(post3);
-    }
+	@Test
+	public void testSave() {
+		try {
+			String a = FileUtil.readFile("C:/Users/SunYukun/Desktop/Redis介绍.md");
+//			System.out.println(a);
+//			postRepository.save(new Post("blog", a, new Date()));
+			Post post = postRepository.findByTitle("blog");
+			System.out.println(post);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
+	}
 }
